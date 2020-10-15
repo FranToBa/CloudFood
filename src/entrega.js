51d1409ce05675d4f9a6a04a5d2dedfce025f5fd
FranToBa <fran.torres.tecnologia@hotmail.com>
2020-10-15 11:11:46 +0200

    Cambiando nombre a src/

---
+++ src/entrega.js
@@ -0,0 +1,8 @@
+export default class Entrega {
+    constructor(ciudad, calle, numero, nombre){
+        this.ciudad = ciudad;
+        this.calle = calle;
+	this.numero = numero;
+	this.nombre = nombre;
+    }
+}
