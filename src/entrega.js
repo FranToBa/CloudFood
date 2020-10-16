06f71e7d03b372223723bf7e55f3ffe1ece9ecf9
FranToBa <fran.torres.tecnologia@hotmail.com>
2020-10-16 21:29:41 +0200

    Borrando codigo innecesario

--- src/entrega.js
+++
@@ -1,8 +0,0 @@
-export default class Entrega {
-    constructor(ciudad, calle, numero, nombre){
-        this.ciudad = ciudad;
-        this.calle = calle;
-	this.numero = numero;
-	this.nombre = nombre;
-    }
-}
