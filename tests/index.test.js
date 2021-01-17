c911620de741091ee705c1f3251ca22285536288
FranToBa <fran.torres.tecnologia@hotmail.com>
2021-01-17 05:18:24 +0100

    Probando tests [skip travis]

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,120 +2,63 @@ var Menu = require ('../src/menu.js');
 const request = require('supertest');
 app = require('../src/index.js');
 
-
-describe("GET /", function() {
-  it("Testeando pagina inicio", function(done) {
+/* EStado */
+describe('GET /status', () => {
+  it('Testando consulta estado', (done) => {
     request(app)
-      .get('/')
-      .expect('Content-Type', "text/html; charset=utf-8")
-      .expect(200,done);
-  });
-});
+      .get('/status')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+})
 
 /* Consulta de platos */
 describe("GET de la carta", function() {
   it("Testeando la consulta de menús", function(done) {
     request(app)
       .get('/carta')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', '/json/')
       .expect(200,done);
   });
   it("Testeando consulta de entrantes", function(done) {
     request(app)
       .get('/carta/entrantes')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
   it("Testeando consulta de platos principales", function(done) {
     request(app)
       .get('/carta/principales')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
   it("Testeando consulta de postres", function(done) {
     request(app)
       .get('/carta/postres')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
 });
 
 
 /* Creación de un menú*/
-describe("PUT /menu/:entrate/:plato/:postre", function() {
+describe("POST /menu", function() {
   it("Testeando creación de un menú con platos no validos", function(done) {
     request(app)
-      .put('/menu/Sopa/Calamares/Fruta')
+      .post('/menu')
+      .set('Accept', 'application/json')
+      .send({entrante:'Sopa', principal:'Patatas',postre: 'Brownie'})
       .expect(400,done);
   });
   it("Testeando creación de un menú", function(done) {
     request(app)
-      .put('/menu/Sopa/Carne/Fruta')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .post('/menu')
+      .set('Accept', 'application/json')
+      .send({entrante:'Sopa', principal:'Carne',postre: 'Brownie'})
+      .expect('Content-Type', /json/)
       .expect(201,done);
   });
 });
-
-/* Consulta del menu seleccionado */
-describe("GET del menu", function() {
-  it("Testeando la consulta del menú seleccionado", function(done) {
-    request(app)
-      .get('/menu/0')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200,done);
-  });
-  it("Testeando la consulta del menú seleccionado que no existe", function(done) {
-    request(app)
-      .get('/menu/100')
-      .expect(400,done);
-  });
-});
-
-/* Modificación de un menú*/
-describe("POST /menu/:id", function() {
-  it("Testeando modificación de menu con plato no disponible", function(done) {
-    request(app)
-      .post('/menu/0')
-      .send({tipo:'entrante', plato:'Patatas'})
-      .expect(400,done);
-  });
-  it("Testeando modificación de un menu con tipo ni disponible", function(done) {
-    request(app)
-      .post('/menu/0')
-      .send({tipo:'aperitivo', plato:'Pasta'})
-      .expect(404,done);
-  });
-  it("Testeando modificación de un menu no existente", function(done) {
-    request(app)
-      .post('/menu/100')
-      .send({tipo:'entrante', plato:'Pasta'})
-      .expect(404,done);
-  });
-  it("Testeando modificación de un menu", function(done) {
-    request(app)
-      .post('/menu/0')
-      .send({tipo:'entrante', plato:'Pasta'})
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200,done);
-  });
-});
-
-
-/* Borrar menu seleccionado */
-describe("DELETE del menu", function() {
-  it("Testeando la consulta de borrado menú no válido", function(done) {
-    request(app)
-      .get('/menu/100')
-      .expect(400,done);
-  });
-  it("Testeando la consulta del menú seleccionado que no existe", function(done) {
-    request(app)
-      .get('/menu/0')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200,done);
-  });
-});
-
 
 
 /* Consulta de precios de la carta o clasificados por tipo*/
@@ -123,13 +66,13 @@ describe("GET /carta/precios", function() {
   it("Testeando consulta de precios", function(done) {
     request(app)
       .get('/carta/precios')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
-  it("Testeando consulta de precios de entrantes", function(done) {
+  it("Testeando consulta de precios de entrantes ", function(done) {
     request(app)
       .get('/carta/precios/entrantes')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
   it("Testeando consulta de precios de tipo que no existe", function(done) {
@@ -144,7 +87,7 @@ describe("GET /carta/precios/:plato", function() {
   it("Testeando consulta de precios de plato específico", function(done) {
     request(app)
       .get('/carta/precios/plato/Carne')
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /json/)
       .expect(200,done);
   });
   it("Testeando consulta de precios de un plato que no existe", function(done) {
@@ -153,5 +96,3 @@ describe("GET /carta/precios/:plato", function() {
       .expect(404,done);
   });
 });
-
-
